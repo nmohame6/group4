93483709067546d5bcb4818032028198ef3f7d31
Hayden Richards <richardshayden12@gmail.com>
2024-08-04 14:47:53 -0400

    feat: implemented promo model

--- api/models/promos.py
+++ api/models/promos.py
@@ -1,13 +1,14 @@
-class PromosCreate(PromosBase):
-    pass
+from sqlalchemy import Column, ForeignKey, Integer, String, DECIMAL, DATETIME
+from sqlalchemy.orm import relationship
+from datetime import datetime
+from ..dependencies.database import Base
 
+class Promo(Base):
+    __tablename__ = "promos"
 
-class PromosUpdate(BaseModel):
-    discount: Optional[int] = None
-    expirydate: Optional[datetime] = None
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    expiration = Column(DATETIME, nullable=False, server_default=str(datetime.now()))
+    code = Column(String, nullable=False)
+    amount = Column(Integer, nullable=False)
 
-class Promo(PromoBase):
-    discount = Optional[int] = None
-
-    class ConfigDict:
-        from_attributes = True
+    payment = relationship("Payment", back_populates="promo")
