b5d5a78e61487f456f5bc2be71bc1d2e19555ef7
Hayden Richards <richardshayden12@gmail.com>
2024-08-04 23:34:00 -0400

    fix: imports edited

--- api/controllers/order_details.py
+++ api/controllers/order_details.py
@@ -1,10 +1,11 @@
 from sqlalchemy.orm import Session
 from fastapi import HTTPException, status, Response, Depends
-from ..models import order_details as model
+from ..models import models, schemas
 from sqlalchemy.exc import SQLAlchemyError
 
+
 def create(db: Session, request):
-    new_item = model.OrderDetail(
+    new_item = models.OrderDetail(
         order_id=request.order_id,
         sandwich_id=request.sandwich_id,
         amount=request.amount
@@ -23,7 +24,7 @@ def create(db: Session, request):
 
 def read_all(db: Session):
     try:
-        result = db.query(model.OrderDetail).all()
+        result = db.query(models.OrderDetail).all()
     except SQLAlchemyError as e:
         error = str(e.__dict__['orig'])
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=error)
@@ -32,7 +33,7 @@ def read_all(db: Session):
 
 def read_one(db: Session, item_id):
     try:
-        item = db.query(model.OrderDetail).filter(model.OrderDetail.id == item_id).first()
+        item = db.query(models.OrderDetail).filter(models.OrderDetail.id == item_id).first()
         if not item:
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Id not found!")
     except SQLAlchemyError as e:
@@ -43,7 +44,7 @@ def read_one(db: Session, item_id):
 
 def update(db: Session, item_id, request):
     try:
-        item = db.query(model.OrderDetail).filter(model.OrderDetail.id == item_id)
+        item = db.query(models.OrderDetail).filter(models.OrderDetail.id == item_id)
         if not item.first():
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Id not found!")
         update_data = request.dict(exclude_unset=True)
@@ -57,7 +58,7 @@ def update(db: Session, item_id, request):
 
 def delete(db: Session, item_id):
     try:
-        item = db.query(model.OrderDetail).filter(model.OrderDetail.id == item_id)
+        item = db.query(models.OrderDetail).filter(models.OrderDetail.id == item_id)
         if not item.first():
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Id not found!")
         item.delete(synchronize_session=False)
