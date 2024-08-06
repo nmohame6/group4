029da7a49ba6a1ecdbe288ac7a5f58af58c7d0d3
Hayden Richards <richardshayden12@gmail.com>
2024-08-05 22:30:26 -0400

    fix: fixed imports

--- api/controllers/promos.py
+++ api/controllers/promos.py
@@ -1,12 +1,14 @@
 from sqlalchemy.orm import Session
-from fastapi import HTTPException, status, Response, Depends
-from ..models import models, schemas
-from sqlalchemy.exc import SQLAlchemyError
+from fastapi import status, Response
+from ..models import models
 
 
 def create(db: Session, promos):
     db_promos = models.Promo(
-        discount=promos.discount
+        promo_id=promos.promo_id,
+        discount = promos.discount,
+        expiration = promos.expiration
+
     )
     db.add(db_promos)
     db.commit()
@@ -14,5 +16,33 @@ def create(db: Session, promos):
     return db_promos
 
 
-def read_models(db: Session, promo_id):
-    return db.query(models.Promo).filter(models.Promo.id == promo_id).all()
+def read_all(db: Session):
+    return db.query(models.Promo).all()
+
+
+def read_one(db: Session, Promo_id):
+    return db.query(models.Promo).filter(models.Promo.id == Promo_id).first()
+
+
+def update(db: Session, Promo_id, promo):
+
+    db_promo = db.query(models.Promo).filter(models.Promo.id == Promo_id)
+
+    update_data = promo.model_dump(exclude_unset=True)
+
+    db_promo.update(update_data, synchronize_session=False)
+
+    db.commit()
+
+    return db_promo.first()
+
+
+def delete(db: Session, promo_id):
+
+    db_promo = db.query(models.Promo).filter(models.Promo.id == promo_id)
+
+    db_promo.delete(synchronize_session=False)
+
+    db.commit()
+
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
