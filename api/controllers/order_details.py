b45bed5b8b314e7ab61deaa0c9afc53422407900
Hayden Richards <richardshayden12@gmail.com>
2024-08-05 22:27:00 -0400

    fix: fixed imports and updated functions

--- api/controllers/order_details.py
+++ api/controllers/order_details.py
@@ -1,6 +1,6 @@
 from sqlalchemy.orm import Session
-from fastapi import HTTPException, status, Response, Depends
-from ..models import models, schemas
+from fastapi import HTTPException, status, Response
+from ..models import models
 from sqlalchemy.exc import SQLAlchemyError
 
 def create(db: Session, request):
